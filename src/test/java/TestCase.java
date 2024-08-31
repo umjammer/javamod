143f754cbf730eac88afa505d8f0f084484e0683
Naohide Sano <namachapanda2005@gmail.com>
2024-09-01 05:16:53 +0900

    add test case

---
+++ src/test/java/TestCase.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright (c) 2024 by Naohide Sano, All rights reserved.
+ *
+ * Programmed by Naohide Sano
+ */
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.concurrent.CountDownLatch;
+
+import de.quippy.javamod.io.SoundOutputStreamImpl;
+import de.quippy.javamod.main.JavaMod;
+import de.quippy.javamod.main.gui.PlayThread;
+import de.quippy.javamod.mixer.Mixer;
+import de.quippy.javamod.multimedia.MultimediaContainer;
+import de.quippy.javamod.multimedia.MultimediaContainerManager;
+import de.quippy.javamod.system.Helpers;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIf;
+import vavi.util.properties.annotation.Property;
+import vavi.util.properties.annotation.PropsEntity;
+
+
+/**
+ * TestCase.
+ *
+ * @author <a href="mailto:umjammer@gmail.com">Naohide Sano</a> (nsano)
+ * @version 0.00 2024-08-27 nsano initial version <br>
+ */
+@PropsEntity(url = "file:local.properties")
+@EnabledIf("localPropertiesExists")
+class TestCase {
+
+    static boolean localPropertiesExists() {
+        return Files.exists(Paths.get("local.properties"));
+    }
+
+    @Property(name = "vavi.test.volume")
+    float volume = 0.2f;
+
+    @Property
+    String mod;
+
+    @BeforeEach
+    void setup() throws Exception {
+        if (localPropertiesExists()) {
+            PropsEntity.Util.bind(this);
+        }
+    }
+
+    @Test
+    @DisplayName("gui")
+    void test1() throws Exception {
+        JavaMod.main(new String[] {mod});
+
+        CountDownLatch cdl = new CountDownLatch(1);
+        cdl.await();
+    }
+
+    @Test
+    @DisplayName("headless")
+    void test2() throws Exception {
+        Helpers.registerAllClasses(); // TODO use service loader
+        MultimediaContainerManager.setIsHeadlessMode(true);
+        MultimediaContainer container = MultimediaContainerManager.getMultimediaContainer(mod);
+        Mixer mixer = container.createNewMixer();
+        mixer.setSoundOutputStream(new SoundOutputStreamImpl());
+        mixer.setVolume(volume);
+        PlayThread playerThread = new PlayThread(mixer, thread -> {}); // TODO extract player from thread
+        playerThread.start();
+
+        CountDownLatch cdl = new CountDownLatch(1);
+        cdl.await();
+    }
+}
